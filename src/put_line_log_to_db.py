6d3fd8a05e04f344ef900563946ecc5fe5334935
sh1un <a0923183408@gmail.com>
2024-04-30 15:56:44 +0800

    feat: finish put Line Log data into DynamoDB

--- src/put_line_log_to_db.py
+++ src/put_line_log_to_db.py
@@ -1,9 +1,12 @@
 import json
 import os
+import uuid
 
 import boto3
 
 s3 = boto3.client("s3")
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("todam_table")
 
 
 def lambda_handler(event, context):
@@ -16,17 +19,42 @@ def lambda_handler(event, context):
     key = event["Records"][0]["s3"]["object"]["key"]
 
     obj = s3.get_object(Bucket=bucket, Key=key)
-    lines = obj["Body"].read().decode("utf-8").splitlines()
+    data = obj["Body"].read().decode("utf-8")
+    data = json.loads(data)
 
     print("Lines:")
-    print(lines)
+    print(data)
+
+    # Extract data
+    message_type = data["events"][0]["message"]["type"]
+    message_id = data["events"][0]["message"]["id"]
+    content = data["events"][0]["message"]["text"]
+    group_id = data["events"][0]["source"]["groupId"]
+    user_id = data["events"][0]["source"]["userId"]
+    send_timestamp = data["events"][0]["timestamp"]
+
+    # Generate UUID
+    random_uuid = str(uuid.uuid4())
+    uuid_no_hyphen = "".join(random_uuid.split("-"))
+
+    # Put data to DynamoDB
+    item = {
+        "id": uuid_no_hyphen,
+        "message_type": message_type,
+        "message_id": message_id,
+        "content": content,
+        "group_id": group_id,
+        "user_id": user_id,
+        "send_timestamp": send_timestamp,
+    }
+
+    table.put_item(Item=item)
 
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
                 "message": "hello S3",
-                # "location": ip.text.replace("\n", "")
             }
         ),
     }
