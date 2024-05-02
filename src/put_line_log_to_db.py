07fd8e0cb7109fd4c4bcc322d8808c743682bfdd
sh1un <a0923183408@gmail.com>
2024-05-02 16:24:58 +0800

    feat: add convert_timestamp_to_utc_plus_8 function

--- src/put_line_log_to_db.py
+++ src/put_line_log_to_db.py
@@ -1,14 +1,30 @@
 import json
 import os
 import uuid
+from datetime import datetime, timedelta, timezone
 
 import boto3
-from boto3.dynamodb.conditions import Attr, Key
+from boto3.dynamodb.conditions import Attr
 
 s3 = boto3.client("s3")
 dynamodb = boto3.resource("dynamodb")
 todam_table = dynamodb.Table("todam_table")
 registered_user_table = dynamodb.Table("registered_user_table")
+
+
+def convert_timestamp_to_utc_plus_8(timestamp: int) -> str:
+    # Convert milliseconds to seconds
+    if timestamp > 1e10:  # Check if timestamp is likely in milliseconds
+        timestamp /= 1000  # Convert from milliseconds to seconds
+
+    utc_time = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+    utc_plus_8 = timezone(timedelta(hours=8))
+    time_in_utc_plus_8 = utc_time.astimezone(utc_plus_8)
+
+    format = "%Y-%m-%d %H:%M:%S"
+    time_in_utc_plus_8 = time_in_utc_plus_8.strftime(format)
+
+    return time_in_utc_plus_8
 
 
 def get_user_type_by_id(user_id: str) -> str:
@@ -89,7 +105,11 @@ def lambda_handler(event, context):
         if items:
             last_item = items[-1]
             last_item["end_timestamp"] = send_timestamp
+            last_item["is_message"] = False
             last_item["is_end"] = True
+            last_item["segment_name"] = (
+                f"{convert_timestamp_to_utc_plus_8(int(last_item['start_timestamp']))}_{convert_timestamp_to_utc_plus_8(int(last_item['end_timestamp']))}"
+            )
             todam_table.put_item(Item=last_item)
 
     return {
